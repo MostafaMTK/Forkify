90cf1cd3c328580f7d04ee41311916fcc0107ccc
Mostafa Mohammed‬‏ <mostafamohammedaz222@gmail.com>
2025-08-29 15:41:36 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '414b7706-9ef8-4a6c-bec3-b5c411e554e3';
+export const KEY = '46684f72-159d-409a-9ae6-1c46d4f51331';
 export const MODAL_CLOSE_SEC = 1.5;
+
